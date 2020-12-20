a92e0cc1938d14ee6db3e1b565682e0f53b769ed
frozensteel1 <FrozenSteel@mail.ru>
2020-12-20 17:11:01 +0300

    Доделал таки файлы через архив

--- app/Http/Livewire/MainTable.php
+++ app/Http/Livewire/MainTable.php
@@ -7,11 +7,13 @@ use App\Models\Worker;
 use App\Models\Division;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\File;
 use Livewire\Component;
 use Livewire\WithFileUploads;
 use Livewire\WithPagination;
-
+use ZipArchive;
+use Illuminate\Support\Facades\Hash;
 
 
 class MainTable extends Component
@@ -23,10 +25,10 @@ class MainTable extends Component
     public $division_name, $division_parent_name, $division_id;
     public $document_name, $document_number, $document_area, $document_id,
         $document_responsible_id, $document_signer_id, $document_tags, $document_date_signing,
-        $document_date_expired, $document_data ,$document_type;
+        $document_date_expired, $document_data, $document_type;
     public $worker_name, $worker_surname, $worker_id, $worker_email, $worker_email_spare, $worker_tel
     , $worker_tel_spare, $worker_position, $worker_patronymic;
-    public $documents, $divisions,$workers;
+    public $documents, $divisions, $workers;
     public $perPage = 10;
     public $search = '';
     public $searchDivision = '';
@@ -39,26 +41,26 @@ class MainTable extends Component
     {
         $this->division_name = '';
         $this->division_parent_name = '';
-        $this->worker_name='';
-        $this->worker_surname='';
-        $this->worker_id='';
-        $this->worker_email='';
-        $this->worker_email_spare='';
-        $this->worker_tel='';
-        $this->worker_tel_spare='';
-        $this->worker_position='';
-        $this->worker_patronymic='';
-        $this->document_name='';
-        $this->document_number='';
-        $this->document_area='';
-        $this->document_id='';
-        $this->document_responsible_id='';
-        $this->document_signer_id='';
-        $this->document_tags='';
-        $this->document_date_signing='';
-        $this->document_date_expired='';
-        $this->document_data='';
-        $this->document_type='';
+        $this->worker_name = '';
+        $this->worker_surname = '';
+        $this->worker_id = '';
+        $this->worker_email = '';
+        $this->worker_email_spare = '';
+        $this->worker_tel = '';
+        $this->worker_tel_spare = '';
+        $this->worker_position = '';
+        $this->worker_patronymic = '';
+        $this->document_name = '';
+        $this->document_number = '';
+        $this->document_area = '';
+        $this->document_id = '';
+        $this->document_responsible_id = '';
+        $this->document_signer_id = '';
+        $this->document_tags = '';
+        $this->document_date_signing = '';
+        $this->document_date_expired = '';
+        $this->document_data = '';
+        $this->document_type = '';
 
 
     }
@@ -71,14 +73,28 @@ class MainTable extends Component
             'document_number' => 'required',
             'document_area' => '',
             'document_data' => '',
-            'document_type'=>'',
+            'document_type' => '',
             'document_date_expired' => '',
             'document_date_signing' => '',
             'document_tags' => '',
             'document_responsible_id' => '',
             'document_signer_id' => '',
         ]);
+        $zip = new ZipArchive;
+        $nameZip = str_replace(['.', '/'], 'w', storage_path() . '\app\docs\\' . Hash::make(implode(getdate()))) . '.zip';
+        if ($zip->open($nameZip, ZipArchive::CREATE) === TRUE) {
 
+            foreach ($validatedDate['document_data'] as $file) {
+
+                $zip->addFile(storage_path() . '\app\livewire-tmp\\' . $file->getFileName(), $file->getClientOriginalName());
+
+            }
+
+            $zip->close();
+        }
+
+
+        $validatedDate['document_data'] = $nameZip;
 
 
         Document::create($validatedDate);
@@ -123,7 +139,7 @@ class MainTable extends Component
     public function storeDivision()
     {
 
-        $rules=[
+        $rules = [
             'division_name' => 'different:division_parent_name|filled|max:255|min:3|required|string',
             'division_parent_name' => 'different:division_name|nullable|max:255|min:3|string',
         ];
@@ -131,7 +147,7 @@ class MainTable extends Component
         Division::create($validatedDate);
         session()->flash('message', 'Подразделение добавлено.');
 
-      //  $this->resetInputFields();
+        //  $this->resetInputFields();
 
         $this->emit('divisionStore'); // Close model to using to jquery
 
@@ -149,6 +165,7 @@ class MainTable extends Component
         $this->division_parent_name = $division->division_parent_name;
 
     }
+
     public function editWorker($id)
     {
         dd('edieditWorkert');
@@ -161,6 +178,7 @@ class MainTable extends Component
         $this->division_parent_name = $division->division_parent_name;
 
     }
+
     public function editDocument($id)
     {
 
@@ -189,6 +207,7 @@ class MainTable extends Component
 
 
     }
+
     public function cancelDocument()
     {
 
@@ -217,8 +236,13 @@ class MainTable extends Component
 
         }
     }
-    public function download($id){
-        dd($id);
+
+    public function download($id)
+    {
+
+        $pathToFile =Document::find($id)['document_data'];
+
+        return response()->download($pathToFile);
     }
 
     public function deleteDivision($id)
@@ -229,6 +253,7 @@ class MainTable extends Component
         }
         $this->emit('documentStore'); // Close model to using to jquery
     }
+
     public function deleteWorker($id)
     {
         if ($id) {
@@ -236,6 +261,7 @@ class MainTable extends Component
             session()->flash('message', 'Работника уволили.');
         }
     }
+
     public function deleteDocument($id)
     {
         if ($id) {
@@ -247,7 +273,6 @@ class MainTable extends Component
     public function searching()
     {
         $this->divisions = Division::where('division_name', 'like', '%' . $this->search . '%')
-
             ->get()
             ->toArray();
     }
@@ -255,29 +280,25 @@ class MainTable extends Component
     public function searchWorkers()
     {
         $this->workers = Worker::where('worker_surname', 'like', '%' . $this->search . '%')
-
             ->get()
             ->toArray();
     }
 
     public function render(): string
     {
-       $this->searching();
-       $this->searchWorkers();
-
-
-
+        $this->searching();
+        $this->searchWorkers();
 
 
         return view('livewire.main-table', [
             'documents' => Document::search($this->search)
-            ->orderBy($this->orderBy, $this->orderAsc ? 'asc' : 'desc')
-            ->simplePaginate($this->perPage),
-            'divisions'=>$this->divisions,
-            'workers'=>$this->workers,
-            'worker_id'=>$this->worker_id,
-            'division_id'=>$this->division_id,
-            'document_id'=>$this->document_id,
+                ->orderBy($this->orderBy, $this->orderAsc ? 'asc' : 'desc')
+                ->simplePaginate($this->perPage),
+            'divisions' => $this->divisions,
+            'workers' => $this->workers,
+            'worker_id' => $this->worker_id,
+            'division_id' => $this->division_id,
+            'document_id' => $this->document_id,
 
 
         ]);
